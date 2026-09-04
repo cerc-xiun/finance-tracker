344a802e0d1d90a2e3760fbdf6ea53854148c30a
cerc-xiun <cercxiun@gmail.com>
2026-09-05 07:17:08 +0800

    refactor: unify schema to single transactions table and implement context managers

--- finance_services.py
+++ finance_services.py
@@ -1,152 +1,141 @@
+from enum import StrEnum
 from database import db_connect
+from psycopg import Error
 
-tables = {
-    'expense' : 'expenses',
-    'income' : 'income'
-}
+invalid_type_message = "The type of record to be updated is neither EXPENSE nor INCOME."
 
-def insert_record( type: str, amount: float, category: str, logged_at: str=None, description: str=None):
-    conn = db_connect()
-    cursor = conn.cursor()
+def insert_record(valid_types: type[StrEnum], type: str, amount: float, category: str, logged_at: str=None, description: str=None, ):
+    if type not in [t.value for t in valid_types]:
+        return {"error" : invalid_type_message}
 
-    table = tables.get(type, None)
-    if table is None:
-        conn.close()
-        return {"error" : "The type of record to be inserted is neither EXPENSE nor INCOME."}
+    try:
+        with db_connect() as conn:
+            cursor = conn.cursor()
+            category_id = _get_category_id(cursor, category)
 
+            if category_id is None:
+                category_id = _create_category(cursor, category)
+                if category_id is None:
+                    return {"error" : "Could not create or retrieve category."}
+            
+            cursor.execute(f"""
+                                INSERT INTO transactions (type, amount, description, category_id, logged_at) VALUES (%s, %s, %s, %s, %s)
+                            """
+                            , (type, amount, description, category_id, logged_at))
+            
+            conn.commit()
+            return {"message" : "Successfully Added the Transaction Record!"}
 
-    category_id = _get_category_id(cursor, category)
-
-    if category_id is None:
-        category_id = _create_category(cursor, category)
-        if category_id is None:
-            conn.close()
-            return {"error" : "Could not create or retrieve category."}
-    
-    cursor.execute(f"""
-                        INSERT INTO {table} (amount, description, category_id, logged_at) VALUES (%s, %s, %s, %s)
-                    """
-                    , (amount, description, category_id, logged_at))
-    
-    conn.commit()
-    conn.close()
-    return {"message" : "Successfully Added the Transaction Record!"}
+    except Error as e:
+        return {"error" : f"Database error: {str(e)}"}
+   
 
 def get_transactions(category: str=None):
-    conn = db_connect()
-    cursor = conn.cursor()
+    try:
+        with db_connect() as conn:
+            cursor = conn.cursor()
 
-    if category is None:
+            if category is None:
 
-        cursor.execute("""  SELECT id, amount, description, category_id, logged_at, created_at, 'expense' AS type
-                            FROM expenses 
-                            
-                            UNION ALL
-                            
-                            SELECT id, amount, description, category_id, logged_at, created_at, 'income' AS type
-                            FROM income  
-                        """)
-        records = cursor.fetchall()
-        conn.close()
+                cursor.execute("""  SELECT id, type, amount, description, category_id, logged_at, created_at
+                                    FROM transactions
+                                """)
 
-        return records
+                records = cursor.fetchall()
+                return records
+            
+            else:
+
+                category_id = _get_category_id(cursor, category)
+                if category_id is None:
+                    return []
+                
+                cursor.execute("""
+                                SELECT id, type, amount, description, category_id, logged_at, created_at
+                                FROM transactions  
+                                WHERE category_id = %s
+                                """, 
+                                (category_id,))
+
+                records = cursor.fetchall()
+                return records
+            
+    except Error as e:
+        return {"error" : f"Database error: {str(e)}"}
     
-    else:
-        category_id = _get_category_id(cursor, category)
-        if category_id is None:
-            conn.close()
-            return []
-        
-        cursor.execute("""
-                        SELECT id, amount, description, category_id, logged_at, created_at, 'expense' AS type
-                        FROM expenses  
-                        WHERE category_id = %s
-                        
-                        UNION ALL
-                        
-                        SELECT id, amount, description, category_id, logged_at, created_at, 'income' AS type
-                        FROM income
-                        WHERE category_id = %s
-                        """, 
-                        (category_id, category_id))
-        records = cursor.fetchall()
-        conn.close()
-        
-        return records
 
-def update_record(id: int, type: str, amount: float=None, description: str=None, logged_at: str=None):
-    conn = db_connect()
-    cursor = conn.cursor()
+def update_record(valid_types: type[StrEnum], id: int, type: str, amount: float=None, description: str=None, logged_at: str=None):
+    if type not in [t.value for t in valid_types]:
+        return {"error" : invalid_type_message}
+    
+    try:
+        with db_connect() as conn:
+            cursor = conn.cursor()
+            
+            if _check_record_existence(cursor=cursor, id=id):
+                updates = []
+                if amount is not None:
+                    cursor.execute(f"""
+                        UPDATE transactions
+                        SET amount = %s
+                        WHERE id = %s
+                    """, (amount, id))
+                    updates.append("Amount")
 
-    table = tables.get(type, None)
-    if table:
-        if _check_record_existence(cursor, id, table):
-            updates = []
-            if amount is not None:
+                if description is not None:
+                    cursor.execute(f"""
+                        UPDATE transactions 
+                        SET description = %s
+                        WHERE id = %s
+                    """, (description, id))
+                    updates.append("Description")
+
+                if logged_at is not None:
+                    cursor.execute(f"""
+                        UPDATE transactions
+                        SET logged_at = %s
+                        WHERE id = %s
+                    """, (logged_at, id))
+                    updates.append("Logged At")
+
+                conn.commit()
+                return {"message" : f"Successfully Updated: {updates}"}
+            
+            else:
+                return {"error" : "Non Existent Record to be updated."}
+    except Error as e:
+        return {"error" : f"Database error: {str(e)}"}
+     
+
+def delete_record(valid_types: type[StrEnum], id: int, type: str):
+    if type not in [t.value for t in valid_types]:
+        return {"error" : invalid_type_message}
+
+    try:
+        with db_connect() as conn:
+            cursor = conn.cursor()
+
+            if _check_record_existence(cursor=cursor, id=id):
                 cursor.execute(f"""
-                    UPDATE {table}
-                    SET amount = %s
+                    DELETE FROM transactions
                     WHERE id = %s
-                """, (amount, id))
-                updates.append("Amount")
-            if description is not None:
-                cursor.execute(f"""
-                    UPDATE {table}
-                    SET description = %s
-                    WHERE id = %s
-                """, (description, id))
-                updates.append("Description")
-            if logged_at is not None:
-                cursor.execute(f"""
-                    UPDATE {table}
-                    SET logged_at = %s
-                    WHERE id = %s
-                """, (logged_at, id))
-                updates.append("Logged At")
+                """, (id,))
 
-            conn.commit()
-            conn.close()
-
-            return {"message" : f"Successfully Updated: {updates}"}
-        
-        else:
-            conn.close()
-            return {"error" : "Non Existent Record to be updated."}
-
-    else:
-        conn.close()
-        return {"error" : "The type of record to be updated is neither EXPENSE nor INCOME."}
-
-def delete_record(id: int, type: str):
-    conn = db_connect()
-    cursor = conn.cursor()
-    table = tables.get(type, None)
-
-    if table:
-
-        if _check_record_existence(cursor, id, table):
-            cursor.execute(f"""
-                DELETE FROM {table}
-                WHERE id = %s
-            """, (id,))
-
-            conn.commit()
-            conn.close()
-
-            return {"message" : f"Successfully deleted record {id}"}
-        
-        else:
-            conn.close()
-            return {"error" : "Non Existent Record to be Deleted."}
-
-    else:
-        conn.close()
-        return {"error" : "The type of record to be deleted is neither EXPENSE nor INCOME."}
-
+                conn.commit()
+                return {"message" : f"Successfully deleted record {id}"}
+            
+            else:
+                return {"error" : "Non Existent Record to be Deleted."}
+    except Error as e:
+        return {"error" : f"Database error: {str(e)}"}
+    
 
 def _get_category_id(cursor, category: str):
-    row = cursor.execute("SELECT id FROM categories WHERE name=%s", (category,)).fetchone()
-    return row['id'] if row else None
+    try:
+        row = cursor.execute("SELECT id FROM categories WHERE name=%s", (category,)).fetchone()
+        return row['id'] if row else None
+    except Error as e:
+        return {"error" : f"Database error: {str(e)}"}
     
 
 def _create_category(cursor, category: str):
@@ -154,10 +143,8 @@ def _create_category(cursor, category: str):
     
     return new_category['id'] if new_category else None
 
-def _check_record_existence(cursor, id: int, table: str) -> bool:
-    cursor.execute(f"SELECT 1 FROM {table} WHERE id= %s", (id,))
 
-    if cursor.fetchone():
-        return True
-    else:
-        return False
+def _check_record_existence(cursor, id: int):
+    cursor.execute(f"SELECT 1 FROM transactions WHERE id= %s", (id,))
+    return cursor.fetchone()
+    
